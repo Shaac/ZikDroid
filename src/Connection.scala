6b6854c0ab31068ffdaabcb958135108ef1744b6
Sacha Delanoue <contact@shaac.me>
2015-08-30 21:20:03 +0200

    Fix license header

--- src/Connection.scala
+++ src/Connection.scala
@@ -12,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this ZikDroid.  If not, see <http://www.gnu.org/licenses/>.
+ * along with ZikDroid.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package me.shaac.zikdroid
